3d01bd3caa50f3fdc4c83d948a0edad5905f3e75
Mace <maceplus@gmail.com>
2018-12-04 00:40:35 +0000

    refactor code

--- src/components/repo-item.vue
+++ src/components/repo-item.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
   <li>
     <h3>{{repository.name}}</h3>
      <p>Description: {{repository.description}}</p> 
@@ -12,11 +11,9 @@
      <p>Has {{repository.open_issues_count}} issues</p>
      </div>
   </li>
-  </div>
 </template> 
 
 <script>
-
 export default {
   name: "repo-item",
   props: ["repository"],
@@ -24,18 +21,12 @@ export default {
     isOpen: false
   })
 };
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
 }
 li {
   display: inline-block;
