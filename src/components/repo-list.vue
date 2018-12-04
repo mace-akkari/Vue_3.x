e9297d17cb3807de7e8ad0f3f94de941bbfc74a1
Mace <maceplus@gmail.com>
2018-12-04 00:41:33 +0000

    new template

---
+++ src/components/repo-list.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <repoItem 
+      v-for="repo in repositories" :key="repo.id"
+      v-bind:repository="repo"
+    >
+    </repoItem>
+  </ul>
+</template>
+
+<script>
+import repoItem from "./repo-item.vue";
+export default {
+  name: "repo-list",
+  components: {
+    repoItem
+  },
+  props: ["repositories"]
+};
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+</style>
